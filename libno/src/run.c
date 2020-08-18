3543584f474650557df6923c5f7e4181630495ae
Matthew Fernandez <matthew.fernandez@gmail.com>
2020-08-17 18:35:30 -0700

    handle the no sandbox case in platform independent code

--- libno/src/run.c
+++ libno/src/run.c
@@ -5,6 +5,7 @@
 #include <no/no.h>
 #include "plat-run.h"
 #include <stddef.h>
+#include <unistd.h>
 
 int no_run(const char **argv, const no_config_t *config) {
 
@@ -16,6 +17,14 @@ int no_run(const char **argv, const no_config_t *config) {
 
   if (config == NULL)
     return EINVAL;
+
+  // if the user requested no limitations, then we can just exec uninstrumented
+  if (config->network && config->home
+      && config->file_system == NO_RESTRICTIONS) {
+    (void)execvp(argv[0], (char*const*)argv);
+
+    return errno;
+  }
 
   return plat_run(argv, config);
 }
