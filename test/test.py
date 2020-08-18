29424bed9c0547244ea6c6046e16f4ae3ede5ddb
Matthew Fernandez <matthew.fernandez@gmail.com>
2020-08-17 18:46:07 -0700

    implement test cases for --[dis]allow-writes

--- test/test.py
+++ test/test.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 
+import os
 import subprocess
+import tempfile
 import unittest
 
 class Test(unittest.TestCase):
@@ -21,6 +23,41 @@ class Test(unittest.TestCase):
     subprocess.check_call(['no', '--allow-network', '--allow-writes', '--',
       'ping', '-c', '1', 'example.com'], stdout=subprocess.DEVNULL)
 
+  def test_allow_writes(self):
+    '''
+    test --allow-writes
+    '''
+
+    with tempfile.TemporaryDirectory() as tmp:
+
+      # try creating a file in this temporary directory
+      foo = os.path.join(tmp, 'foo')
+      p = subprocess.run(['no', '--allow-network', '--allow-writes', '--',
+        'touch', foo], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+      self.assertEqual(p.returncode, 0)
+
+      # this should also succeed with network access disallowed
+      p = subprocess.run(['no', '--allow-writes', '--', 'touch', foo],
+        stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+      self.assertEqual(p.returncode, 0)
+
+      # create a scratch file
+      bar = os.path.join(tmp, 'bar')
+      with open(bar, 'w') as f:
+        f.write('hello world\n')
+
+      # try appending to this file
+      p = subprocess.run(['no', '--allow-network', '--allow-writes', '--',
+        'tee', '-a', bar], input='an extra line\n', stdout=subprocess.DEVNULL,
+        stderr=subprocess.DEVNULL, universal_newlines=True)
+      self.assertEqual(p.returncode, 0)
+
+      # this should also succeed with network access disallowed
+      p = subprocess.run(['no', '--allow-writes', '--', 'tee', '-a', bar],
+        input='an extra line\n', stdout=subprocess.DEVNULL,
+        stderr=subprocess.DEVNULL, universal_newlines=True)
+      self.assertEqual(p.returncode, 0)
+
   def test_disallow_network(self):
     '''
     test disallowing network access (the default)
@@ -40,6 +77,41 @@ class Test(unittest.TestCase):
       self.fail('ping succeeded with network access denied')
     except subprocess.CalledProcessError:
       pass
+
+  def test_disallow_writes(self):
+    '''
+    test disallowing file system writes (the default)
+    '''
+
+    with tempfile.TemporaryDirectory() as tmp:
+
+      # try creating a file in this temporary directory
+      foo = os.path.join(tmp, 'foo')
+      p = subprocess.run(['no', '--allow-network', '--', 'touch', foo],
+        stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+      self.assertNotEqual(p.returncode, 0)
+
+      # this should also fail with network access disallowed
+      p = subprocess.run(['no', '--', 'touch', foo],
+          stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+      self.assertNotEqual(p.returncode, 0)
+
+      # create a scratch file
+      bar = os.path.join(tmp, 'bar')
+      with open(bar, 'w') as f:
+        f.write('hello world\n')
+
+      # try appending to this file
+      p = subprocess.run(['no', '--allow-network', '--', 'tee', '-a', bar],
+        input='an extra line\n', stdout=subprocess.DEVNULL,
+        stderr=subprocess.DEVNULL, universal_newlines=True)
+      self.assertNotEqual(p.returncode, 0)
+
+      # this should also fail with network access disallowed
+      p = subprocess.run(['no', '--', 'tee', '-a', bar],
+        input='an extra line\n', stdout=subprocess.DEVNULL,
+        stderr=subprocess.DEVNULL, universal_newlines=True)
+      self.assertNotEqual(p.returncode, 0)
 
 if __name__ == '__main__':
   unittest.main()
