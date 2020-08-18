5e25e70d7196c6dd37c74023ea438aca854137b2
Matthew Fernandez <matthew.fernandez@gmail.com>
2020-08-17 18:34:42 -0700

    support other macOS built-in sandbox profiles

--- libno/src/macos/exec.c
+++ libno/src/macos/exec.c
@@ -11,12 +11,9 @@ int plat_run(const char **argv, const no_config_t *config) {
   assert(argv[0] != NULL);
   assert(config != NULL);
 
-  // TODO: implement other restrictions
-  if (!config->home || config->file_system != NO_RESTRICTIONS)
-    return ENOTSUP;
-
-  // can we use the built-in "no network" profile?
-  if (!config->network) {
+  // can we use the built-in “no network” profile?
+  if (!config->network && config->home
+      && config->file_system == NO_RESTRICTIONS) {
     char *err;
 #pragma clang diagnostic push
 #pragma clang diagnostic ignored "-Wdeprecated-declarations"
@@ -25,6 +22,35 @@ int plat_run(const char **argv, const no_config_t *config) {
 #pragma clang diagnostic pop
       return ENOSYS;
     }
+
+  // can we use the built-in “no write” profile?
+  } else if (config->network && config->home
+      && config->file_system == NO_WRITES) {
+    char *err;
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Wdeprecated-declarations"
+    if (sandbox_init(kSBXProfileNoWrite, SANDBOX_NAMED, &err) != 0) {
+      sandbox_free_error(err);
+#pragma clang diagnostic pop
+      return ENOSYS;
+    }
+
+  // can we use the built-in “no write except temporary” profile?
+  } else if (config->network && config->home
+      && config->file_system == NO_WRITES_EXCEPT_TMP) {
+    char *err;
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Wdeprecated-declarations"
+    if (sandbox_init(kSBXProfileNoWriteExceptTemporary, SANDBOX_NAMED, &err)
+        != 0) {
+      sandbox_free_error(err);
+#pragma clang diagnostic pop
+      return ENOSYS;
+    }
+
+  // TODO: implement other restrictions
+  } else {
+    return ENOTSUP;
   }
 
   (void)execvp(argv[0], (char*const*)argv);
